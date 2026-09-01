e5886823f258dacec182f2a534b76fba8b001087
shubham kumar <rajashubham993@gmail.com>
2026-09-01 23:58:47 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -75,16 +75,12 @@ WSGI_APPLICATION = 'rastaurant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'restaurant_db',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-
-    },
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
