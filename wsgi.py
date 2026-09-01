286b031bb9065f81861d7d9c55d4b94234df07e1
shubham kumar <rajashubham993@gmail.com>
2026-09-02 00:05:24 +0530

    Reformat wsgi.py for consistency

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,7 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rastaurant.settings')
 
 application = get_wsgi_application()
-app = application  # Yeh line Vercel ke liye bahut zaroori hai
+app = application  # Vercel ke liye zaroori hai
