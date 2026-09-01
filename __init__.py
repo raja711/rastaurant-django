bdafad8e7adea8b0f57731b43593fee96a2efcd8
shubham kumar <rajashubham993@gmail.com>
2026-09-01 20:38:54 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,3 @@
+import django.db.backends.base.base
+
+django.db.backends.base.base.BaseDatabaseWrapper.check_database_version_supported = lambda self: None
---
+++ routers.py
@@ -0,0 +1,18 @@
+class BookingRouter:
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'myapp' and model.__name__ == 'Booking':
+            return 'booking_db'
+        return None
+
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'myapp' and model.__name__ == 'Booking':
+            return 'booking_db'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        return True
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if model_name == 'booking':
+            return db == 'booking_db'
+        return db == 'default'
