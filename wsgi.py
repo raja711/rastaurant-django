f3a1c3b3bed80ff378b8335afb52f30455ecb5ae
shubham kumar <rajashubham993@gmail.com>
2026-09-02 00:59:59 +0530

    Reorder imports and application setup in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-
 application = get_wsgi_application()
-app = application  # Vercel ke liye zaroori hai
+app = application
