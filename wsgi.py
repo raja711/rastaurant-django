c89e152122493cd11f8445ed5201bb24a1918240
shubham kumar <rajashubham993@gmail.com>
2026-09-02 00:26:06 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rastaurant.settings')
-
 application = get_wsgi_application()
 app = application  # Vercel ke liye zaroori hai
