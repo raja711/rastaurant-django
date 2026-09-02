eb405ccd6269e003ae3f655bb5531ad91a5e4b72
raja711 <rajashubham993@gmail.com>
2026-09-02 17:24:08 +0530

    Update staticfiles path and permissions

--- settings.py
+++ settings.py
@@ -112,13 +112,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+# Static files (CSS, JavaScript, Images)
 import os
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# settings.py ke bilkul bottom mein paste karein:
