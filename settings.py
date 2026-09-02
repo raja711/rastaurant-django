de0fc72cfaab0ff157cbec88399fee7e4b02d9d8
raja711 <rajashubham993@gmail.com>
2026-09-02 20:34:49 +0530

    Fix static files styling on Vercel

--- settings.py
+++ settings.py
@@ -119,3 +119,4 @@ import os
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'myapp/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
